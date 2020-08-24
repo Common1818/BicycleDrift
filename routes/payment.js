5e9b54a3ad6c7c4746f432a4ccfba8f28a57233d
Priya-Bihani <priyabihani123@gmail.com>
2020-08-24 15:10:31 +0530

    Payment

--- routes/payment.js
+++ routes/payment.js
@@ -14,7 +14,9 @@ const nodemailer = require('nodemailer');
 var gmailEmail = 'marketingacad.help@gmail.com';
 var gmailPassword = 'abcd9876';
 const mailTransport = nodemailer.createTransport({
-   service: 'gmail',
+   host: 'smtp.gmail.com',
+   port: 465,
+   secure: true,
    auth: {
       user: gmailEmail,
       pass: gmailPassword,
