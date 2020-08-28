a39c8554c30666173b12fa567d300b86e1f3c00c
Priya-Bihani <priyabihani123@gmail.com>
2020-08-28 21:05:47 +0530

    Filter done

--- client/src/components/products/Products.js
+++ client/src/components/products/Products.js
@@ -1,92 +1,110 @@
-import React, { useEffect, useState } from "react";
-import { connect } from "react-redux";
-import { Row, Col, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { getAllBikes } from "../../actions/products";
-import { emptyProductState } from "../../actions/product";
-import Loader from "../layout/Loader";
-import ProductCard from "./ProductCard";
-import Pagination from "./Pagination";
-import "./css/Products.css";
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { Row, Col, Button } from 'react-bootstrap';
+
+import { getAllBikes } from '../../actions/products';
+import { getAllCategories } from '../../actions/category';
+import { emptyProductState } from '../../actions/product';
+import { searchResult } from '../../actions/products';
+
+import Filter from './Filter';
+import Loader from '../layout/Loader';
+import ProductCard from './ProductCard';
+import Pagination from './Pagination';
+import './css/Products.css';
 
 const Products = ({
-  getAllBikes,
-  emptyProductState,
-  bikes,
-  accessories,
-  searchResults,
-  loading,
-  match,
+   getAllBikes,
+   getAllCategories,
+   emptyProductState,
+   bikes,
+   accessories,
+   categories,
+   searchResults,
+   searchResult,
+   loading,
+   match,
 }) => {
-  useEffect(() => {
-    getAllBikes();
-    emptyProductState();
-  }, []);
+   useEffect(() => {
+      getAllBikes();
+      getAllCategories();
+      emptyProductState();
+   }, []);
 
-  const type = match.params.type;
+   const type = match.params.type;
 
-  const products =
-    type === "search"
-      ? searchResults.length === 0
-        ? bikes
-        : searchResults
-      : type === "bikes"
-      ? bikes
-      : accessories;
+   const products =
+      type === 'search'
+         ? searchResults.length === 0
+            ? bikes
+            : searchResults
+         : type === 'bikes'
+         ? bikes
+         : accessories;
 
-  console.log("products: ", products);
+   console.log('products: ', products);
 
-  const heading =
-    type === "search"
-      ? "Search Results..."
-      : type === "bikes"
-      ? "Bicycles..."
-      : "Accessories...";
+   const heading =
+      type === 'search'
+         ? 'Search Results...'
+         : type === 'bikes'
+         ? 'Bicycles...'
+         : 'Accessories...';
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const [productsPerPage] = useState(5);
+   const [currentPage, setCurrentPage] = useState(1);
+   const [productsPerPage] = useState(5);
 
-  // Get current products
-  const indexOfLastProduct = currentPage * productsPerPage;
-  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-  const currentProducts = products.slice(
-    indexOfFirstProduct,
-    indexOfLastProduct
-  );
+   // Get current products
+   const indexOfLastProduct = currentPage * productsPerPage;
+   const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+   const currentProducts = products.slice(
+      indexOfFirstProduct,
+      indexOfLastProduct
+   );
 
-  // Change page
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+   // Change page
+   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-  return loading ? (
-    <Loader />
-  ) : (
-    <div className="products-container">
-      <div className="add-to-cart-gif">
-        <img src="" alt="" srcset="" />
+   return loading ? (
+      <Loader />
+   ) : (
+      <div className="products-container">
+         <div className="add-to-cart-gif">
+            <img src="" alt="" srcset="" />
+         </div>
+         <h1>{heading}</h1>
+         {type === 'search' || type === 'bikes' ? (
+            <div className="filter">
+               <Filter categories={categories} searchResult={searchResult} />
+            </div>
+         ) : null}
+
+         <Row className="wrapper">
+            {currentProducts &&
+               currentProducts.map((item) => (
+                  <ProductCard product={item} key={item._id} />
+               ))}
+         </Row>
+         <Pagination
+            productsPerPage={productsPerPage}
+            totalProducts={products.length}
+            paginate={paginate}
+         />
       </div>
-      <h1>{heading}</h1>
-      <Row className="wrapper">
-        {currentProducts &&
-          currentProducts.map((item) => (
-            <ProductCard product={item} key={item._id} />
-          ))}
-      </Row>
-      <Pagination
-        productsPerPage={productsPerPage}
-        totalProducts={products.length}
-        paginate={paginate}
-      />
-    </div>
-  );
+   );
 };
 
 const mapStateToProps = (state) => ({
-  bikes: state.products.Bikes,
-  accessories: state.products.Accessories,
-  searchResults: state.products.SearchResults,
-  loading: state.products.loading,
+   bikes: state.products.Bikes,
+   accessories: state.products.Accessories,
+   searchResults: state.products.SearchResults,
+   loading: state.products.loading,
+   categories: state.category.categories,
 });
 
-export default connect(mapStateToProps, { getAllBikes, emptyProductState })(
-  Products
-);
+export default connect(mapStateToProps, {
+   getAllBikes,
+   emptyProductState,
+   getAllCategories,
+   searchResult,
+})(Products);
